63eae4b165f67b3c219f8e42ab0af9fc30dc7e77
Petter Holmström <petter@vaadin.com>
2019-05-23 08:42:04 +0300

    Move cursor between tokens by clicking with the mouse

--- token-field.js
+++ token-field.js
@@ -103,7 +103,7 @@ class TokenField extends LitElement {
             if (token.length > 0) {
                 if (this._isInlineEditor()) {
                     this.addTokenAtPosition(token, this.inlineEditorPosition);
-                    this._moveEditor(this.inlineEditorPosition +1);
+                    this._moveEditor(this.inlineEditorPosition + 1);
                 } else {
                     this.addToken(token);
                 }
@@ -112,10 +112,10 @@ class TokenField extends LitElement {
             event.preventDefault();
         } else if (event.key === 'Backspace' && cursorAtTheStart && editorIsEmpty) {
             if (this._isInlineEditor()) {
-                this.removeTokenAtPosition(this.inlineEditorPosition -1);
-                this._moveEditor(this.inlineEditorPosition -1);
+                this.removeTokenAtPosition(this.inlineEditorPosition - 1);
+                this._moveEditor(this.inlineEditorPosition - 1);
             } else {
-                this.removeTokenAtPosition(this.tokens.length -1);
+                this.removeTokenAtPosition(this.tokens.length - 1);
             }
             event.preventDefault();
         } else if (event.key === 'Delete' && cursorAtTheEnd && editorIsEmpty && this._isInlineEditor()) {
@@ -141,9 +141,40 @@ class TokenField extends LitElement {
         }
         const editor = this._getEditor();
         if (event.target !== editor) {
+            if (this._isOnToken(event)) {
+                this._moveEditor(this._getPositionOfTokenElement(event.target));
+            } else if (this._isBetweenTokens(event)) {
+                this._moveEditor(this._getPositionOfTokenToTheLeft(event) + 1);
+            }
             editor.focus();
             event.preventDefault();
         }
+    }
+
+    _isBetweenTokens(mouseEvent) {
+        return mouseEvent.target.id == 'editorContainer';
+    }
+
+    _isOnToken(mouseEvent) {
+        return mouseEvent.target.classList.contains('token');
+    }
+
+    _getPositionOfTokenElement(tokenElement) {
+        return this.indexOf(tokenElement.dataset.token);
+    }
+
+    _getPositionOfTokenToTheLeft(mouseEvent) {
+        const tokenElements = this.shadowRoot.querySelectorAll(".token");
+        let position = -1;
+        for (let i = 0; i < tokenElements.length; ++i) {
+            const tokenX = tokenElements[i].getBoundingClientRect().left;
+            if (tokenX <= mouseEvent.clientX) {
+                position = i;
+            } else {
+                break;
+            }
+        }
+        return position;
     }
 
     addToken(token) {
@@ -187,7 +218,7 @@ class TokenField extends LitElement {
 
     _createToken(token, index) {
         return html`
-            <div class="token" style="order: ${index * 2}">${token}</div>
+            <div class="token" style="order: ${index * 2}" data-token="${token}">${token}</div>
         `;
     }
 
@@ -197,15 +228,15 @@ class TokenField extends LitElement {
 
     _moveEditorLeft() {
         if (this.inlineEditorPosition == -1) {
-            this._moveEditor(this.tokens.length -1);
+            this._moveEditor(this.tokens.length - 1);
         } else if (this.inlineEditorPosition > 0) {
-            this._moveEditor(this.inlineEditorPosition -1);
+            this._moveEditor(this.inlineEditorPosition - 1);
         }
     }
 
     _moveEditorRight() {
         if (this.inlineEditorPosition > -1) {
-            this._moveEditor(this.inlineEditorPosition +1);
+            this._moveEditor(this.inlineEditorPosition + 1);
         }
     }
 
@@ -214,7 +245,7 @@ class TokenField extends LitElement {
 
         if (this.tokens.length > 0 && position > -1 && position < this.tokens.length) {
             console.log(`Moving editor to position ${position}`);
-            editor.style.order = (position * 2) -1;
+            editor.style.order = (position * 2) - 1;
             editor.classList.add('inline');
             this.inlineEditorPosition = position;
         } else if (this.inlineEditorPosition > -1) {
@@ -263,5 +294,13 @@ class TokenField extends LitElement {
         return this.shadowRoot.getElementById('canvas');
     }
 }
+
+// TODO: Move editor to default position when the token field is focused
+// TODO: Change order of tokens by dragging and dropping them
+// TODO: Select tokens from list of predefined tokens, optionally allowing nonexistent tokens and even adding new ones to the list
+// TODO: Test in Safari and FireFox
+// TODO: Themeing
+// TODO: Vaadin Flow integration
+// TODO: Select multiple tokens with mouse and with keyboard
 
 customElements.define('token-field', TokenField);
